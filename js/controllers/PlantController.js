127c44f05d3a42a6cbb215f38ce3156ca7298d8a
Roger <roger@apis3.com>
2022-02-27 11:36:52 -0300

    getting json

--- js/controllers/PlantController.js
+++ js/controllers/PlantController.js
@@ -14,11 +14,34 @@ export default class PlantController {
 
     onChange(){
 
-        [...this.formEl.elements].forEach(function(field, index){
-            field.addEventListener('change',function(){
-                console.log('trocou?');
+        this.getOrder();
+
+    }
+
+    getOrder(){        
+
+        [...this.formEl.elements].forEach((field, index) => {
+            field.addEventListener('change',() => {
+                console.log(field.value);
+                this.getValues(field.value);
             });
         });
+
+    }
+
+    getValues(){
+
+        const options = {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'default'
+        }
+
+        fetch(`https://front-br-challenges.web.app/api/v2/green-thumb/?sun=high&water=regularly&pets=false`, options)
+        .then(res => {res.json()
+            .then(data=> console.log(data))
+        })
+        .catch(e=>console.log('deu erro: ' + e.message));
 
     }
 
