f251f6993ac95004b1c36df10a09684245867b9a
Roger <roger@apis3.com>
2022-02-27 12:11:19 -0300

    setting order by dropdowns

--- js/controllers/PlantController.js
+++ js/controllers/PlantController.js
@@ -18,18 +18,33 @@ export default class PlantController {
 
     }
 
-    getOrder(){        
+    getOrder(){  
 
         [...this.formEl.elements].forEach((field, index) => {
             field.addEventListener('change',() => {
-                console.log(field.value);
-                this.getValues(field.value);
-            });
+                this.getValues();
+            });            
         });
 
+        
     }
 
     getValues(){
+
+        let selectOrder = [];
+
+        [...this.formEl.elements].forEach((field, index) => {
+            if (field.name == 'sun' && field.value == ''){
+                field.value = 'high';
+            } else if (field.name == 'water' && field.value == ''){
+                field.value = 'regularly';
+            } else if (field.name == 'pet' && field.value == ''){
+                field.value = 'false';
+            }
+            selectOrder.push(field.value);
+        });    
+
+        console.log(selectOrder);
 
         const options = {
             method: 'GET',
@@ -37,7 +52,7 @@ export default class PlantController {
             cache: 'default'
         }
 
-        fetch(`https://front-br-challenges.web.app/api/v2/green-thumb/?sun=high&water=regularly&pets=false`, options)
+        fetch(`https://front-br-challenges.web.app/api/v2/green-thumb/?sun=${selectOrder[0]}&water=${selectOrder[1]}&pets=${selectOrder[2]}`, options)
         .then(res => {res.json()
             .then(data=> console.log(data))
         })
